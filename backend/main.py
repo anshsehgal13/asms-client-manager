05e2c24bfa53d69d31967c42759d56f9bd180bd4
anshsehgal13 <anshsehgal03@gmail.com>
2026-04-30 20:15:36 +0530

    fixes for deployment

--- backend/main.py
+++ backend/main.py
@@ -27,11 +27,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "https://asms-client-manager-frontend.onrender.com",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
