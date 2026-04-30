ee68ddcc31b990de450437fb87f8f08b043045ce
anshsehgal13 <anshsehgal03@gmail.com>
2026-04-30 19:57:06 +0530

    fixes for deployment

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,7 +4,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://asms-client-manager.onrender.com/api',
+  baseURL: 'https://asms-client-manager.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 })
 
